312761f856808a11477d2dfc885d3217aa7a3a0e
Christian Motta <chrismotta2005@gmail.com>
2026-09-02 10:05:04 -0300

    Update default filler mode to auto

--- src/machines/filler/FillerPanel.java
+++ src/machines/filler/FillerPanel.java
@@ -101,7 +101,7 @@ public class FillerPanel extends JFrame {
               // later be fed by the real turntable/orchestrator without the two racing.
               final JRadioButton amode = new JRadioButton("Auto");
               final JRadioButton mmode = new JRadioButton("Manual");
-              mmode.setSelected(true);
+              amode.setSelected(true);
               java.awt.event.ActionListener modeToggle = new java.awt.event.ActionListener() {
                       public void actionPerformed(java.awt.event.ActionEvent e) {
                               boolean manual = mmode.isSelected();
@@ -139,6 +139,7 @@ public class FillerPanel extends JFrame {
               panel.setVisible(true);
               panel.defaultRatioButton.doClick();
               panel.defaultVolumeButton.doClick();
+              for (JRadioButton b : panel.devButtons) b.setEnabled(false);
 
               SignalServer<FillerVizWorker> server = new SignalServer<FillerVizWorker>(Ports.PORT_FILLER_VIZ, FillerVizWorker.class);
               new Thread(server).start();
